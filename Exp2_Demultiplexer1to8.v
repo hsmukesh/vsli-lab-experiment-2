18327f078a6c36d75eba1b4ab2c5139e59897233
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 12:14:34 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,1 +1,17 @@
-
+module demux(s,d,y);
+input d;
+input [2:0]s;
+output[7:0]y;
+wire w1,w2,w3;
+not g1(w1,s[0]);
+not g2(w2,s[1]);
+not g3(w3,s[2]);
+and g4(y[0],d,w1,w2,w3);
+and g5(y[1],d,w1,s[0],w3);
+and g6(y[2],d,w3,s[1],w1);
+and g7(y[3],d,s[0],s[1],w3);
+and g8(y[4],d,s[2],w1,w2);
+and g9(y[5],d,s[2],s[0],w2);
+and g10(y[6],d,w1,s[1],s[2]);
+and g11(y[7],d,s[2],s[1],s[0]);
+endmodule
