264684c7c9109ea05ad7dcd17386af51b1ba7def
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 12:15:52 +0530

    Update Exp2_magnitudecomparator.v

--- Exp2_magnitudecomparator.v
+++ Exp2_magnitudecomparator.v
@@ -1,1 +1,25 @@
-
+module magnitude(a,b,great,less,equal);
+input[1:0]a,b;
+output reg great,less,equal;
+always@(*)
+begin
+if(a>b)
+begin 
+great=1'b1;
+less=1'b0;
+equal=1'b0;
+end 
+else if(a<b)
+begin
+great=1'b0;
+less=1'b1;
+equal=1'b0;
+end 
+else 
+begin
+great=1'b0;
+less=1'b0;
+equal=1'b1;
+end 
+end 
+endmodule
